2f40710b2d0cb4033b8513ffe6fef789a3bc0bd2
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-08-26 10:53:02 -0500

    feat: separate template from the logic

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ from fastapi import UploadFile
 
 # HTML response
 from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 
 # Shutil for files
 import shutil
 
+# Unique code
+import uuid
+
+
+
+# Start FastAPI
 app = FastAPI()
 
+# Templates dir
+templates = Jinja2Templates(directory="templates")
+
 # Home
-@app.get('/')
-async def get_home():
-    content = """
-    <html>
-        <body>
-            <h3>Upload a photo</h3>
-            <form action="/photo/" enctype="multipart/form-data" method="post">
-                <input name="file" type="file">
-                <input type="submit">
-            </form>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=content)
+@app.get('/', response_class=HTMLResponse)
+async def get_home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
     
 # Photo
 @app.post("/photo/")
 async def upload_photo(file: UploadFile = File(...)):
-    # Guardar el archivo en el servidor
+    # Save the file (Temp)
     file_location = f"uploads/{file.filename}"
     with open(file_location, "wb") as f:
         shutil.copyfileobj(file.file, f)
