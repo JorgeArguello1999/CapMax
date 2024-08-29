2816dfffba2fc2b114cd2e05d55c5b1bc2d92abc
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-08-29 10:31:25 -0500

    feat: RUC_function

--- modules/text_recognise.py
+++ modules/text_recognise.py
@@ -6,40 +6,40 @@ from typing import List
 """ Recognize differents items, depends that you need
 """
 
-def rucs_detects(text:str="") -> list:
+def rucs_detects(text: str = "") -> list:
     """RUC Detect
-    Ruc is a item wit 10 numbers or maybe 13
-    This function is only for Ecuador Country
+    RUC is an item with 10 or 13 numbers.
+    This function is only for Ecuador Country.
     
     Keyword arguments:
-    text: (str) All text for recognize
-    Return: (list) Items with answer
+    text: (str) All text for recognition.
+    Return: (list) Items with RUCs in order of appearance.
     """
     # Clean data from letters, spaces or symbols 
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     text = re.sub('\n', '', text)
 
-    # Ruc from proveedor or costumer is on first part
-    large = len(text)//3
-    text = text[:large*2]
+    # RUC from provider or customer is usually on the first part
+    large = len(text) // 3
+    text = text[:large * 2]
 
-    # REGEX item with 10-13 items large
+    # REGEX items with 10-13 characters length
     items_10_13 = re.findall(r'\d{10,13}', text) 
 
-    # REGEX item with 13 items large and 001 code for ruc
-    ruc_numbers = re.findall(r'\b\d{10}001\b', text)
-
-    # Mix result
-    results = set(ruc_numbers + items_10_13)
-
-    prefix = [ f'0{i}' for i in range(1, 10) ] + [ f'{i}' for i in range(11, 25) ] + [ '88', '90' ]
+    prefix = [f'0{i}' for i in range(1, 10)] + [f'{i}' for i in range(11, 25)] + ['88', '90']
     suffix = '001'
 
     # Classify Items with 10 or 13 
-    results = [ x for x in results if (
+    results = [ x for x in items_10_13 if (
         (len(x) == 10 or len(x) == 13) and 
         (x.startswith(tuple(prefix)) and x.endswith(suffix))
-    ) ]
+    )]
+
+   # Eliminate duplicates while maintaining order
+    seen = set()
+    results = [item for item in results if (
+        item not in seen and not seen.add(item)
+    )]
 
     return results
 
