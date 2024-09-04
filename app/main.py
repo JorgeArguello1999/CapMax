e04d8b9bdfd9081150d19ef752444c666eec3056
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-09-04 17:05:00 -0500

    feat: New field for deposit photos

--- app/main.py
+++ app/main.py
@@ -58,12 +58,13 @@ async def get_home(request: Request):
     
 # Photo
 @app.post("/photo/")
-async def upload_photo(file: UploadFile = File(...), ia:Optional[bool]=Form(False)):
+async def upload_photo(file: UploadFile = File(...), ia:Optional[bool]=Form(False), deposit:Optional[bool]=Form(False)):
     # Save the file 
     response, file_location = photo.save(file)
 
     # Process photo
     print(f'>>> IA use: {ia}')
+    print(f'>>> Deposit mode: {deposit}')
     process = photo.process(file_location, ia)
 
     # Delete photo
