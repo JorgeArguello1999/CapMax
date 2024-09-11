1db94e58e4d491d62a47c75bac6ec39c33ec2e0a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-09-11 18:16:46 -0500

    feat: qualifier for deposit_detect is function

--- handlers/deposit_detect.py
+++ handlers/deposit_detect.py
@@ -1,13 +1,42 @@
 from concurrent.futures import ThreadPoolExecutor
 
+from vision import gpt_recognise
 from modules import deposit_recognise as dpr
 
-def get_response(text:str, file_path:str) -> dict:
+def get_response(text: str, file_path: str) -> dict:
     regex = regex_response(text)
-    # Create a qualifier answer
-    return regex
 
-def regex_response(text:str) -> dict:
+    score = calculate_score(
+        amount=regex['amount'], 
+        receipt_number=regex['receipt_number'], 
+        destination_account=regex['destination_account'], 
+        name=regex['name'], 
+        date=regex['date']
+    )
+
+    result = regex
+    if score < 3:
+        print(f">>> Using GPT results... Score [{score}]")
+        result = gpt_recognise.process_image(file_path)
+    else:
+        print(f">>> Using REGEX results... Score [{score}]")
+
+    return result
+
+# Función para calificar las respuestas obtenidas por REGEX
+def calculate_score(amount, receipt_number, destination_account, name, date):
+    score = 5  
+
+    if not amount: score -= 1
+    if not receipt_number: score -= 1
+    if not destination_account: score -= 1
+    if not name: score -= 1
+    if not date: score -= 1
+
+    return score
+
+# Obtener los datos usando REGEX con el uso de múltiples hilos
+def regex_response(text: str) -> dict:
     with ThreadPoolExecutor() as executor:
         amount = executor.submit(dpr.extract_amount, text=text)
         receipt_number = executor.submit(dpr.extract_receipt_number, text=text)
@@ -20,5 +49,6 @@ def regex_response(text:str) -> dict:
             'receipt_number': receipt_number.result(),
             'destination_account': destination.result(),
             'name': name.result(),
-            'date': date.result()
+            'date': date.result(),
+            'ai': False  
         }
